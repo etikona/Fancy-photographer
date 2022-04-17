652a6068ac915a3c6a32abb28c35114ca0996a24
eti kona <etikonapal@gmail.com>
2022-04-17 21:24:26 +0600

    secured checkout page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Blogs from './Pages/Blogs/Blogs';
 import CheckOut from './Pages/CheckOut/CheckOut';
 import Login from './Pages/Login/Login';
 import Notfound from './Pages/Notfound/Notfound';
+import RequiredAuth from './Pages/RequiredAuth/RequiredAuth';
 import Signin from './Pages/Signin/Signin';
 import Footer from './Shared/Footer/Footer';
 import Header from './Shared/Header/Header';
@@ -21,7 +22,11 @@ function App() {
        <Route path="/signin" element={<Signin></Signin>} />
        <Route path="/login" element={<Login></Login>} />
        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
-      <Route path="/checkout" element={<CheckOut></CheckOut>}></Route>
+      <Route path="/checkout" element={
+        <RequiredAuth>
+          <CheckOut></CheckOut>
+        </RequiredAuth>
+      }></Route>
       <Route path="/about" element={<AboutMe></AboutMe>}></Route>
       <Route path="*" element={<Notfound></Notfound>}></Route>
      </Routes>
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+    }
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(email, password);
+    }
     return (
         <div>
-            <form >
+            <form onSubmit={handleLogin} >
                 <div className='container' >
                     <h3>Login</h3>
                     <h6>Email</h6>
-                    <input type="email" placeholder='your email' />
+                    <input type="email" placeholder=' email' onChange={handleEmail} />
                     <h6>Password</h6>
-                    <input type="password" placeholder='Type your password' />
+                    <input type="password" placeholder=' password' onChange={handlePassword} />
                     <br />
-                    <button className='btn btn-dark mt-3 w-50' type="submit">Login</button>
+                    <button className='btn btn-dark mt-5 w-75' type="submit">Login</button>
                 </div>
             </form>
         </div>
