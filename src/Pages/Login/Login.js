9664f0b1aa757c5761218c48b25bff05b85479a7
eti kona <etikonapal@gmail.com>
2022-04-17 19:16:05 +0600

    login and signin form created

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './Login.css';
 
 const Login = () => {
     return (
         <div>
-            <h2>Log In</h2>
+            <form >
+                <div className='container' >
+                    <h3>Login</h3>
+                    <h6>Email</h6>
+                    <input type="email" placeholder='your email' />
+                    <h6>Password</h6>
+                    <input type="password" placeholder='Type your password' />
+                    <br />
+                    <button className='btn btn-dark mt-3 w-50' type="submit">Login</button>
+                </div>
+            </form>
         </div>
     );
 };
--- src/Pages/Signin/Signin.js
+++ src/Pages/Signin/Signin.js
@@ -3,7 +3,19 @@ import React from 'react';
 const Signin = () => {
     return (
         <div>
-            <h5>Sing In your name</h5>
+            <form >
+                <div className='container' >
+                    <h3>Signin</h3>
+                    <h6>Email</h6>
+                    <input type="email" placeholder='email' />
+                    <h6>Password</h6>
+                    <input type="password" placeholder=' password' />
+                    <br />
+                    <h6>Confirm password</h6>
+                    <input type="password" placeholder='Confirm passwotd' />
+                    <button className='btn btn-dark mt-3 w-50' type="submit">Signin</button>
+                </div>
+            </form>
         </div>
     );
 };
