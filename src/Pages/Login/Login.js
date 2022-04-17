7f99503b0db9df810d9b2aa5bf35157411329258
eti kona <etikonapal@gmail.com>
2022-04-18 05:47:02 +0600

    error handle in login and sign in

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,28 +1,63 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Login.css';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [userInfo, setUserInfo] = useState({
+        email: "",
+        password: "",
+
+    })
+    const [errors, setErrors] = useState({
+        email: "",
+        password: "",
+        other: ""
+    })
     const [
         signInWithEmailAndPassword,
         user,
         loading,
-        error,
+        hookError,
     ] = useSignInWithEmailAndPassword(auth);
+
+    // Email
     const handleEmail = (e) => {
-        setEmail(e.target.value)
+        const emailRegax = /\S+@\S+\.\S+/;
+        const validEmail = emailRegax.test(e.target.value);
+        if (validEmail) {
+            setUserInfo({ ...userInfo, email: e.target.value })
+            setErrors({ ...errors, email: "" })
+        }
+        else {
+            setErrors({ ...errors, email: "Invalid email id" })
+            setUserInfo({ ...userInfo, email: "" })
+        }
     }
+    // Password 
     const handlePassword = (e) => {
-        setPassword(e.target.value);
+        const passwordRegax = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+        const validPassword = passwordRegax.test(e.target.value);
+        if (validPassword) {
+            setUserInfo({ ...userInfo, password: e.target.value })
+            setErrors({ ...errors, password: "" })
+        }
+        else {
+            setErrors({ ...errors, password: "wrong password" });
+            setUserInfo({ ...errors, password:"" })
+        }
+
     }
 
     const handleLogin = (e) => {
         e.preventDefault();
-        signInWithEmailAndPassword(email, password);
+        signInWithEmailAndPassword(userInfo.email, userInfo.password);
     }
+    useEffect( ()=>{
+        console.log(hookError)
+    }, [hookError])
+
     return (
         <div>
             <form onSubmit={handleLogin} >
@@ -30,10 +65,15 @@ const Login = () => {
                     <h3>Login</h3>
                     <h6>Email</h6>
                     <input type="email" placeholder=' email' onChange={handleEmail} />
+                    {errors?.email && <p className='error-msg'>{errors.email}</p>}
                     <h6>Password</h6>
                     <input type="password" placeholder=' password' onChange={handlePassword} />
+                    {errors?.password && <p className='error-msg'>{errors.password}</p>}
                     <br />
-                    <button className='btn btn-dark mt-5 w-75' type="submit">Login</button>
+                    <button className='btn btn-dark mt-3 w-75' type="submit">Login</button>
+                    <p> <Link to="/signin">Sign in first</Link></p>
+                    {/* {error && <p className='error-msg'>{error}</p>} */}
+                    {hookError && <p>{hookError?.message}</p>}
                 </div>
             </form>
         </div>
--- src/Pages/Signin/Signin.js
+++ src/Pages/Signin/Signin.js
@@ -1,19 +1,91 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Signin.css'
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Signin = () => {
+    const [userInfo, setUserInfo] = useState({
+        email: "",
+        password: "",
+        confirmPassword: ""
+    })
+    const [errors, setErrors] = useState({
+        email: "",
+        password: "",
+        other: ""
+    })
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        hookError,
+    ] = useCreateUserWithEmailAndPassword(auth, {sendEmailVerification: true});
+
+    // Email
+    const handleEmail = (e) => {
+        const emailRegax = /\S+@\S+\.\S+/;
+        const validEmail = emailRegax.test(e.target.value);
+        if (validEmail) {
+            setUserInfo({ ...userInfo, email: e.target.value })
+            setErrors({ ...errors, email: "" })
+        }
+        else {
+            setErrors({ ...errors, email: "Invalid email id" })
+            setUserInfo({ ...userInfo, email: "" })
+        }
+    }
+    // Password 
+    const confirmPassword = (e) => {
+
+        if (e.target.value === userInfo.password) {
+            setUserInfo({ ...userInfo, confirmPassword: e.target.value })
+            setErrors({ ...errors, password: "" })
+        }
+        else {
+            setErrors({ ...errors, password: "please set correct password" });
+            setUserInfo({ ...userInfo, confirmPassword: "" })
+        }
+
+    }
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        createUserWithEmailAndPassword(userInfo.email, userInfo.password);
+    }
+    useEffect(() => {
+        console.log(hookError)
+    }, [hookError])
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || "/";
+
+    useEffect(() => {
+        if (user) {
+
+            navigate(from)
+        }
+    }, [user])
+
     return (
         <div>
-            <form >
+            <form onSubmit={handleLogin} >
                 <div className='container' >
-                    <h3>Signin</h3>
+                    <h3>Sign in</h3>
                     <h6>Email</h6>
-                    <input type="email" placeholder='email' />
+                    <input type="email" placeholder=' email' onChange={handleEmail} />
+                    {errors?.email && <p className='error-msg'>{errors.email}</p>}
                     <h6>Password</h6>
-                    <input type="password" placeholder=' password' />
+                    <input type="text" placeholder=' password' />
                     <br />
-                    <h6>Confirm password</h6>
-                    <input type="password" placeholder='Confirm passwotd' />
-                    <button className='btn btn-dark mt-3 w-50' type="submit">Signin</button>
+                    <h6>Confirm Password</h6>
+                    <input type="password" placeholder='Confirm password' onChange={confirmPassword} />
+                    {errors?.password && <p className='error-msg'>{errors.password}</p>}
+                    <br />
+                    <button className='btn btn-dark mt-5 w-75' type="submit">Sign in</button>
+                    {/* {error && <p className='error-msg'>{error}</p>} */}
+                    {hookError && <p>{hookError?.message}</p>}
                 </div>
             </form>
         </div>
